63f78e6258efc95c7c9ae821b8bf60f434160be5
Gustav Tonér <gustav.toner@gmail.com>
2020-10-10 18:35:27 +0200

    Add expand button with initial details

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { useSortBy, useTable } from 'react-table'
+import { useExpanded, useSortBy, useTable } from 'react-table'
+import { Button } from 'rebass';
 import { Home } from '../types/Home'
 import './Table.scss'
 
@@ -9,6 +10,16 @@ import './Table.scss'
  
    const columns = React.useMemo<any>(
      () => [
+      {
+        // Make an expander cell
+        Header: () => null, // No header
+        id: 'expander', // It needs an ID
+        Cell: ({ row }: any) => (
+          <span {...row.getToggleRowExpandedProps()}>
+            {row.isExpanded ? '➖' : '➕'}
+          </span>
+        ),
+      },
        { Header: "Adress", accessor: "address" },
        { Header: "Stad", accessor: "cityName" },
        { Header: "Rum", accessor: "rooms", },
@@ -16,9 +27,33 @@ import './Table.scss'
        { Header: "Våning", accessor: "floor", },
        { Header: "Tillträde", accessor: "entryDate", },
        { Header: "Hyra", accessor: "rent", },
+       {
+        // Make an expander cell
+        Header: () => null, // No header
+        id: 'info', // It needs an ID
+        Cell: ({ row }: any) => (
+          <span>
+            <Button>Mer info</Button>
+          </span>
+        ),
+      }
      ],
      []
-   )
+   );
+
+     // Create a function that will render our row sub components
+  const renderRowSubComponent = React.useCallback(
+    ({ row }) => (
+      <pre
+        style={{
+          fontSize: '10px',
+        }}
+      >
+        <code>{JSON.stringify({ values: row.values }, null, 2)}</code>
+      </pre>
+    ),
+    []
+  );
  
    const {
      getTableProps,
@@ -26,8 +61,10 @@ import './Table.scss'
      headerGroups,
      rows,
      prepareRow,
+     visibleColumns,
    } = useTable({ columns, data },
-    useSortBy
+    useSortBy,
+    useExpanded
     )
  
    return (
@@ -56,6 +93,7 @@ import './Table.scss'
          {rows.map(row => {
            prepareRow(row)
            return (
+             <>
              <tr {...row.getRowProps()}>
                {row.cells.map(cell => {
                  return (
@@ -67,6 +105,21 @@ import './Table.scss'
                  )
                })}
              </tr>
+             {row.isExpanded ? (
+                  <tr>
+                    <td colSpan={visibleColumns.length}>
+                      {/*
+                          Inside it, call our renderRowSubComponent function. In reality,
+                          you could pass whatever you want as props to
+                          a component like this, including the entire
+                          table instance. But for this example, we'll just
+                          pass the row
+                        */}
+                      {renderRowSubComponent({ row })}
+                    </td>
+                  </tr>
+                ) : null}
+             </>
            )
          })}
        </tbody>
