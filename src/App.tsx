7e8e1393f84dd32ee6950d0153defd621fdee328
Linnea Daisy <linnea.d.karlsson@gmail.com>
2020-10-10 16:30:41 +0200

    Init layout and themeing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import { ReactQueryDevtools } from 'react-query-devtools';
 import { useQuery } from 'react-query';
+import { ThemeProvider } from 'emotion-theming'
+import theme from './theme';
+
+import {
+  Heading,
+  Box,
+} from 'rebass';
 import './App.css';
 import { BostadsregistretHome } from './types/BostadsregistretHome';
 
@@ -10,18 +17,59 @@ function App() {
   const { data, isLoading } = useQuery<BostadsregistretHome[]>('apartments', () => fetch(bostadsregistretDataUrl).then(res => res.json()));
 
   return (
-    <>
-    <div className="App">
-      <header className="App-header">
-        <ul>
+    <ThemeProvider theme={theme}>
+      <Box
+  sx={{
+    display: 'grid',
+    minHeight: '100vh',
+    gridTemplateAreas: [
+      '"header" "nav" "main"  "footer"',
+      '"header header" "nav main" "footer footer"'
+    ],
+    gridTemplateColumns: [
+      '1fr',
+      '200px 1fr'
+    ],
+    gridTemplateRows: [
+      'min-content min-content 1fr min-content',
+      'min-content 1fr'
+    ]
+  }}>
+  <Box
+  bg='pd'
+    sx={{
+      gridArea: 'header'
+    }}>
+    <Heading>JKPGBo</Heading>
+  </Box>
+  <Box
+  bg='base'
+    sx={{
+      gridArea: 'main'
+    }}>
+      <ul>
           { isLoading ? <p>Loading!</p> :
             data?.map((home: BostadsregistretHome) => (<li key={home.id}>{home.address}</li>))
           }
         </ul>
-      </header>
-    </div>
-    <ReactQueryDevtools />
-    </>
+        <ReactQueryDevtools />
+  </Box>
+  <Box
+  bg='p'
+    sx={{
+      gridArea: 'nav'
+    }}>
+    Filter
+  </Box>
+  <Box
+  bg='pd'
+    sx={{
+      gridArea: 'footer'
+    }}>
+    Footer
+  </Box>
+</Box>
+    </ThemeProvider>
   );
 }
 
